bbf299aea46ae6f53174972f6e0825d9eaf3a517
Preston Roe <pjroe@sbcglobal.net>
2025-06-14 14:07:09 -0500

    added to scratch pad to keep track of and verify work, updated allowed hosts to any

---
+++ SQL/scratch_pad.sql
@@ -0,0 +1,7 @@
+SELECT * FROM pokemon;
+SELECT * FROM pokemon WHERE pokemon_name ILIKE '%QUI%';
+
+SELECT * FROM regions;
+SELECT * FROM regions WHERE region_name ILIKE '%nov%';
+
+SELECT * FROM versions;
