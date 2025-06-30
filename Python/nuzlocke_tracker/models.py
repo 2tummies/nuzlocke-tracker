b0fc8cd10b8b1f7b030eef07856143e6916f21cb
Preston Roe <pjroe@sbcglobal.net>
2025-06-30 09:53:05 -0500

    updated models file to match database

--- Python/nuzlocke_tracker/models.py
+++ Python/nuzlocke_tracker/models.py
@@ -51,7 +51,7 @@ class PokemonEncounter(models.Model):
     location = models.ForeignKey(Location, models.DO_NOTHING)
     pokemon = models.ForeignKey(Pokemon, models.DO_NOTHING)
     method = models.ForeignKey(EncounterMethod, models.DO_NOTHING)
-    version_exclusive = models.ForeignKey(Version, models.DO_NOTHING, blank=True, null=True)
+    version_exclusive = models.SmallIntegerField()
     time_of_day = models.ForeignKey(TimeOfDay, models.DO_NOTHING, blank=True, null=True)
     percent_encounter = models.SmallIntegerField()
     class Meta:
