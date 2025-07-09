822abf93e3f7acf545d2bf5397012101a0d9f574
Preston Roe <pjroe@sbcglobal.net>
2025-07-09 13:34:20 -0500

    add guidance and info to where things should be added for urason

--- React/src/components/App.js
+++ React/src/components/App.js
@@ -1,7 +1,11 @@
+// components
+// TODO: import component here, use this format ' import ComponentName from './FileName' '
+
 const App = () => {
   return (
     <>
       <div>Nuzlock Tracker App</div>
+      {/* TODO: add component here, use this format ' <ComponentName /> ' */}
     </>
   )
 }
