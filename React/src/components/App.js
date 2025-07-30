59790c928906ee71ca9605e10914b6a2af7da462
Ryan <ryanenriquez65@hotmail.com>
2025-07-29 19:20:30 -0600

    Added PageHeader component and restructured App.js

--- React/src/components/App.js
+++ React/src/components/App.js
@@ -1,12 +1,12 @@
 // components
 // TODO: import component here, use this format ' import ComponentName from './FileName' '
+import PageHeader from "./PageHeader"
 
 const App = () => {
   return (
     <>
-      <div>Nuzlock Tracker App</div>
       {/* TODO: add component here, use this format ' <ComponentName /> ' */}
-      <div>Created by: 2tummies and Urason</div>
+      {<PageHeader> </PageHeader>}
     </>
   )
 }
